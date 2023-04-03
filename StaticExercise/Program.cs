93f0722c2d5664109a231ea4cbd4204c2c3c28ef
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-04-03 12:08:42 -0500

    added notes and a loop

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool? exit = null;
+            while (exit == null)
+            {
+                Console.WriteLine("would you like to covert celcius to fahrenheit or fehrenheit to celcius?");
+                var path = Console.ReadLine();
+                switch (path.ToLower())
+                {
+                    case "yes":
+                        Console.WriteLine("if you want to convert celcius to fahrenheit press 1\nif you like to convert fahrenheit to celcius press 2");
+                        var path2 = int.Parse(Console.ReadLine());
+                        switch (path2)
+                        {
+                            case 1: Console.WriteLine("enter celcius degree.");
+                                var degree = double.Parse(Console.ReadLine());
+                                double answer = TempConverter.CelToFah(degree);
+                                Console.WriteLine($"{degree} celcius is {answer} degrees fahrenheit");
+                                Console.WriteLine("press Enter to continue");
+                                Console.ReadLine();
+                                Console.Clear();
+                                continue;
+
+                            case 2: Console.WriteLine("enter fahrenheit degree.");
+                                degree = double.Parse(Console.ReadLine());
+                                answer = TempConverter.FahToCel(degree);
+                                Console.WriteLine($"{degree} fahrenheit is {answer} degrees celcius");
+                                Console.WriteLine("press Enter to continue");
+                                Console.ReadLine();
+                                Console.Clear();
+                                continue;
+                            default:
+                                Console.WriteLine("not an option");
+                                exit = true;
+                                break;
+                        }
+                        break;
+                    case "no":
+                        exit = true;
+                        break;
+                }
+            }
+
+
+
+
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahToCel(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public static double CelToFah(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+
+    }
+}
